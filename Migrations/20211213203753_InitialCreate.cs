074f0d0b1105b7043b012c64dd82ef985689eb00
shroukAhmedElshafey <96087483+shroukAhmedElshafey@users.noreply.github.com>
2021-12-14 00:25:27 +0200

    Add files via upload

---
+++ Migrations/20211213203753_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeDotNetCoreCrud.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    EmployeeName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    EmployeeCode = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    position = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    OfficeLocation = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
